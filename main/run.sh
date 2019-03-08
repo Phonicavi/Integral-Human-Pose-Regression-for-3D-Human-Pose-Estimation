5bcd4d7b9e93fa258bcfb20a3941602900380018
Phonicavi <mardpazin7san@hotmail.com>
2019-03-09 03:23:23 +0800

    fix script

--- main/run.sh
+++ main/run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
 srun --partition=Pose --gres=gpu:8 --job-name=Integral python main.py --gpu 0-7 --continue
+#srun --partition=Pose --gres=gpu:6 --job-name=Integral python main.py --gpu 0-5 --baseline
