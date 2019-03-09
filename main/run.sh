f2f76b43010f15876f654d3c3e19c7d6a4589d3a
Phonicavi <mardpazin7san@hotmail.com>
2019-03-09 17:30:31 +0800

    ign modified

--- main/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-srun --partition=Pose --gres=gpu:8 --job-name=Integral python main.py --gpu 0-7 --continue
-#srun --partition=Pose --gres=gpu:6 --job-name=Integral python main.py --gpu 0-5 --baseline
